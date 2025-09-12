1283527006e29c588bcc83c69db4f3a48213f7ff
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-09-12 16:54:46 -0700

    update

--- Lab_7.cpp
+++ Lab_7.cpp
@@ -1,5 +1,15 @@
-#include<iostream>
-using namespace std;
+#include<iostream>     // COMSC-210 | Lab 5 | Mansoor Noori
+using namespace std;   // IDE Visual Studio,
+
+// function to display the element of Arrays
+void displayArray(string* arr, int size){
+    for (int i = 0; i < size; i++){
+         cout << *(arr + i) << " ";
+    }
+   cout << endl;
+
+}
+
 
 int main(){
 
