4d84eadd2120e8c6e409306becd714c2fbd99b8e
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-09-12 17:04:00 -0700

    update

--- Lab_7.cpp
+++ Lab_7.cpp
@@ -10,8 +10,23 @@ void displayArray(string* arr, int size){
 
 }
 
-
+// Function to reverse the elements of the array
+string* reverseArray(string* arr, int size) {
+    for (int i = 0; i < size / 2; i++) {
+        string temp = *(arr + i);
+        *(arr + i) = *(arr + size - 1 - i);
+        *(arr + size - 1 - i) = temp;
+    }
+    return arr;
+}
 int main(){
+    int size = 5;
+    
+    // Dynamically allocate string array
+    string* names = new string[size];
+
+    // feed the array with names 
+    
 
 
     cout << "hello" << endl;
